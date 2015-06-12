9adc35031e682f6d7ef87a6ea296c0c540e069f1
Szymon Konefal <szymon.konefal@intel.com>
2015-06-12 16:42:41 +0200

    remove dead code

--- bus/bus_socket.cpp
+++ bus/bus_socket.cpp
@@ -51,23 +51,5 @@ namespace mesos {
 namespace serenity {
 
 
-//Try<Nothing> BusSocket::registration(std::string topic){
-//  return Nothing();
-//}
-//
-//
-////skonefal TODO: Waiting for serenity.proto generic serenity event
-//Try<Nothing> BusSocket::subscribe(
-//    std::string topic,
-//    std::function<void(mesos::scheduler::Event)> callback) {
-//  return Nothing();
-//}
-//
-//
-////skonefal TODO: Waiting for serenity.proto generic serenity event
-//Try<Nothing> BusSocket::publish(std::string topic, mesos::scheduler::Event event){
-//  return  Nothing();
-//}
-
 } // namespace serenity
 } // namespace mesos
--- serenity/serenity.hpp
+++ serenity/serenity.hpp
@@ -59,18 +59,7 @@ namespace serenity {
 // The bus socket allows peers to communicate (subscribe and publish)
 // asynchronously.
 class BusSocket {
-public:
-//  // Filters need to claim a topic before being able to
-//  // publish to it.
-//  Try<Nothing> registration(std::string topic);
-//
-//  // NOTE: Subscribe can return a future instead of relying
-//  // on a provided callback.
-//  Try<Nothing> subscribe(
-//      std::string topic,
-//      std::function<void(mesos::scheduler::Event)> callback); //skonefal TODO: Waiting for serenity.proto generic serenity event
-//
-//  Try<Nothing> publish(std::string topic, mesos::scheduler::Event event); //skonefal TODO: Waiting for serenity.proto generic serenity event
+
 };
 
 
