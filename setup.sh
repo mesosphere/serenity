8c8b1da9c8c720315a1af5cc7ffa3afce32196c2
Szymon Konefal <szymon.konefal@intel.com>
2015-06-16 15:25:27 +0200

    Unzip flags change

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 mkdir build #2> /dev/null
 #if it's already unpacked then ignore error.
-unzip -u -n lib/pbjson.zip -d lib/
-unzip -u -n -f lib/gmock-1.7.0.zip -d lib/
+unzip -n lib/pbjson.zip -d lib/
+unzip -n lib/gmock-1.7.0.zip -d lib/
