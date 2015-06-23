00683da52560c518a492d052d1ffb1cad8ad1a26
Niklas Q. Nielsen <nik@qni.dk>
2015-06-23 16:31:23 -0700

    Updated lint script

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python scripts/cpplint.py --filter="-legal/copyright" $( find . -name "*.cpp" -or -name "*.hpp" | grep -v -e "build/" -e "lib/" )
+python scripts/cpplint.py --extensions="hpp,cpp" --filter="-legal/copyright" $( find . -name "*.cpp" -or -name "*.hpp" | grep -v -e "build/" -e "lib/" )
