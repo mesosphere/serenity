85bea90d73276dbed8be7b00cff34ec71594a50e
bplotka <bwplotka@gmail.com>
2015-07-31 12:08:00 -0400

    Improved framework script.
    Signed-off-by: bplotka <bwplotka@gmail.com>

--- framework/run_framework.sh
+++ framework/run_framework.sh
@@ -2,6 +2,7 @@
 
 JSON_PATH=$1
 
-source /opt/serenity/mesos/etc/environment
+source /opt/serenity/mesos/etc/mesos-slave
+export MESOS_MASTER=$MESOS_MASTER
 
 ./test-framework --logging_level=INFO --json_path="${JSON_PATH}"
