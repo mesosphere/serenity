feed53e00850e6b8bbd04f0db53b3ac955581728
bplotka <bwplotka@gmail.com>
2015-08-17 19:11:30 -0400

    Moved QoS Controller to use IPC.
    Signed-off-by: bplotka <bwplotka@gmail.com>

--- qos_controller/serenity_controller_module.cpp
+++ qos_controller/serenity_controller_module.cpp
@@ -21,6 +21,7 @@
 using namespace mesos;  // NOLINT(build/namespaces)
 
 using mesos::serenity::ChangePointDetectionState;
+using mesos::serenity::CpuQoSPipeline;
 using mesos::serenity::IpsQoSPipeline;
 using mesos::serenity::RollingFractionalDetector;
 using mesos::serenity::SerenityController;
@@ -29,7 +30,57 @@ using mesos::serenity::QoSPipelineConf;
 
 using mesos::slave::QoSController;
 
-static QoSController* createSerenityController(const Parameters& parameters) {
+static QoSController* createIpcSerenityController(
+    const Parameters& parameters) {
+  LOG(INFO) << "Loading Serenity QoS Controller module";
+  // TODO(bplotka): Fetch configuration from parameters or conf file
+  // to customize IpsDrop
+
+  // --Hardcoded configuration for Serenity QoS Controller---
+
+  QoSPipelineConf conf;
+  ChangePointDetectionState cpdState;
+  // Detector configuration:
+  // How far we look back in samples.
+  cpdState.windowSize = 10;
+  // How many iterations detector will wait with creating another
+  // contention.
+  cpdState.contentionCooldown = 10;
+  // Defines how much (relatively to base point) value must drop to trigger
+  // contention.
+  // Most detectors will use that.
+  cpdState.fractionalThreshold = 0.5;
+  // Defines how to convert difference in values to CPU.
+  // This option helps RollingFractionalDetector to estimate severity of
+  // drop.
+  cpdState.differenceToCPU = 0.4;  // 0.4 IPC drop means ~ 1 CPU to kill.
+
+  conf.cpdState = cpdState;
+  conf.emaAlpha = 0.8;
+  conf.visualisation = true;
+  // Let's start with QoS pipeline disabled.
+  conf.valveOpened = false;
+
+  // Since slave is configured for 5 second perf interval, it is useless to
+  // check correction more often then 5 sec.
+  double onEmptyCorrectionInterval = 5;
+
+  // --End of hardcoded configuration for Serenity QoS Controller---
+
+  Try<QoSController*> result =
+      SerenityController::create(
+          std::shared_ptr<QoSControllerPipeline>(
+              new CpuQoSPipeline<RollingFractionalDetector>(conf)),
+          onEmptyCorrectionInterval);
+
+  if (result.isError()) {
+    return NULL;
+  }
+  return result.get();
+}
+
+static QoSController* createIpsSerenityController(
+    const Parameters& parameters) {
   LOG(INFO) << "Loading Serenity QoS Controller module";
   // TODO(bplotka): Fetch configuration from parameters or conf file
   // to customize IpsDrop
@@ -85,4 +136,4 @@ mesos::modules::Module<QoSController> com_mesosphere_mesos_SerenityController(
     "support@mesosphere.com",
     "Serenity QoS Controller",
     NULL,
-    createSerenityController);
+    createIpcSerenityController);
