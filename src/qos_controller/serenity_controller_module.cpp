00553dc5842f2f19deeeb202b034bf5895e82938
bplotka <bwplotka@gmail.com>
2015-08-20 04:57:16 -0700

    Adjusting severity.
    Signed-off-by: bplotka <bwplotka@gmail.com>

--- src/qos_controller/serenity_controller_module.cpp
+++ src/qos_controller/serenity_controller_module.cpp
@@ -51,7 +51,7 @@ static QoSController* createIpcSerenityController(
   // contention.
   // Most detectors will use that.
   cpdState.fractionalThreshold = 0.3;
-  cpdState.severityLevel = 0.4;
+  cpdState.severityLevel = 1.2;
 
   conf.cpdState = cpdState;
   conf.emaAlpha = 0.9;
@@ -98,7 +98,7 @@ static QoSController* createIpsSerenityController(
   // contention.
   // Most detectors will use that.
   cpdState.fractionalThreshold = 0.5;
-  cpdState.severityLevel = 0.1;
+  cpdState.severityLevel = 1;
 
   conf.cpdState = cpdState;
   conf.emaAlpha = 0.8;
