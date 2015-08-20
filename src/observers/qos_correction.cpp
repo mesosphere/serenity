2fdd9a0b16864c388c030671def6b43650f3fcdb
bplotka <bwplotka@gmail.com>
2015-08-20 03:11:14 -0700

    Adding debug log.
    Signed-off-by: bplotka <bwplotka@gmail.com>

--- src/observers/qos_correction.cpp
+++ src/observers/qos_correction.cpp
@@ -197,9 +197,9 @@ Try<QoSCorrections> SeverityBasedSeniorityDecider::decide(
   // Get ages for executors.
   list<pair<double, ResourceUsage_Executor>> executors;
   for (const ResourceUsage_Executor& executor : possibleAggressors) {
-
+    LOG(INFO) << "Before getting age";
     Try<double> age = ageFilter->age(executor.executor_info());
-
+    LOG(INFO) << "After getting age";
     if (age.isError()) {
       LOG(WARNING) << age.error();
       continue;
