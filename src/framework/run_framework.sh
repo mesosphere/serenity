f6ffb465b814b2012c673342f6af8a0db676b28e
bplotka <bwplotka@gmail.com>
2015-10-29 20:01:28 +0100

    Extend Smoke Test Framework with additional options. TODO: Add support for unlimited jobs.
    Signed-off-by: bplotka <bwplotka@gmail.com>

--- src/framework/run_framework.sh
+++ src/framework/run_framework.sh
@@ -2,6 +2,8 @@
 
 JSON_PATH=$1
 
+# NOTE(bplotka) valid for serenity deployment using
+# serenity-formulas (https://github.com/Bplotka/serenity-formula)
 source /opt/serenity/mesos/etc/mesos-slave
 export MESOS_MASTER=$MESOS_MASTER
 
