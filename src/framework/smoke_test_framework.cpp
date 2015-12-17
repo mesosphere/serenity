0690e0fba746e1c3536f7565f097fce7ef20e2a2
bplotka <bwplotka@gmail.com>
2015-12-17 17:44:51 +0100

    Temporarly disable influxdb storing.
    Signed-off-by: bplotka <bwplotka@gmail.com>

--- src/framework/smoke_test_framework.cpp
+++ src/framework/smoke_test_framework.cpp
@@ -258,8 +258,9 @@ class SerenityNoExecutorScheduler : public Scheduler
         record.setTag(TsTag::TASK_ID, status.task_id().value());
         record.setTag(TsTag::EXECUTOR_ID, status.executor_id().value());
         record.setTag(TsTag::HOSTNAME, task->second); //get hostname
-        LOG(INFO) << "Sending data about preempted task to InfluxDB.";
-        dbBackend->PutMetric(record);
+        LOG(INFO) << "Sending data about preempted task to InfluxDB is "
+                       "disabled.";
+        //dbBackend->PutMetric(record);
       }
     } else {
       LOG(INFO) << "Task '" << status.task_id() << "'"
