cb3e5548ef119e4f97df3c942df73dd19e711c7a
bplotka <bwplotka@gmail.com>
2016-01-21 13:11:50 +0100

    Added extra logs.
    Signed-off-by: bplotka <bwplotka@gmail.com>

--- src/filters/correction_merger.hpp
+++ src/filters/correction_merger.hpp
@@ -38,7 +38,9 @@ class CorrectionMergerFilter:
     const std::vector<QoSCorrections> products) {
     QoSCorrections corrections;
 
+    uint64_t receivedCententionNum = 0;
     for (QoSCorrections product : products) {
+      receivedCententionNum += product.size();
       for (slave::QoSCorrection correction : product) {
         if (checkForDuplicates(correction, corrections)) {
           // Filter out duplicated value.
@@ -48,6 +50,9 @@ class CorrectionMergerFilter:
         corrections.push_back(correction);
       }
     }
+
+    SERENITY_LOG(INFO) << "Received " << receivedCententionNum << " corrections"
+      << "and merged to " << corrections.size() << " corrections.";
 
     produce(corrections);
 
