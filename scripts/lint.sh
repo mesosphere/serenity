d10d4bc9dd6baeaa5c7ee6d1f8ebf1c5827824c1
bplotka <bwplotka@gmail.com>
2016-02-08 15:15:40 +0100

    Fix for CMT enablement.
    Additionally fixed lint script to be more readable (moved to whitelist from blacklist)
    
    
    Signed-off-by: bplotka <bwplotka@gmail.com>

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,7 +3,10 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 SERENITY_ROOT=$( dirname $(readlink -e "${DIR}/"))
 
+# Run lint on all src/* cpp/hpp files except smoke_test_framework source yet.
 python ${DIR}/cpplint.py \
   --extensions="hpp,cpp" \
   --filter="-legal/copyright" \
-$( find "${SERENITY_ROOT}/" -name "*.cpp" -or -name "*.hpp" | grep -v -e "${SERENITY_ROOT}/build/" -e "${SERENITY_ROOT}/src/mesos_frameworks/smoke_test/" -e "${SERENITY_ROOT}/3rdparty/" )
+$( find "${SERENITY_ROOT}/" -name "*.cpp" -or -name "*.hpp" | \
+	grep -e "${SERENITY_ROOT}/src/" | \
+	grep -v -e "${SERENITY_ROOT}/src/mesos_frameworks/smoke_test/")
